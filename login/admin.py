6a711a48c809642b6f52a201374d709d716bccc8
HIMANSHI SHRIVAS <44641920+himanshishrivas@users.noreply.github.com>
2021-04-16 19:02:35 +0530

    Add files via upload

---
+++ login/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Login
+
+admin.site.register(Login)
+#admin.site.register(Choice)
+
